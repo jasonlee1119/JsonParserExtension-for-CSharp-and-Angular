61c4d99b3ab11f2d76c113c6d91d08762d9a8704
jasonlee1119 <147239482+jasonlee1119@users.noreply.github.com>
2023-10-09 09:02:46 +0800

    Implement converting functions

--- json-parser/src/app/app.component.ts
+++ json-parser/src/app/app.component.ts
@@ -10,9 +10,14 @@ export class AppComponent implements OnInit{
   jsonString: string = '';
   accessorString = '{ get; set; }';
   modifierString = 'public';
+  dateTimeOffsetPattern = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?\+\d{2}:\d{2}$/;
+  dateTimePattern = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?$/;
+  utcDateTiimePattern =/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?Z$/;
 
   inputForm = this.fb.group({
-    jsonStringInput: this.fb.control<string>('', {validators: [Validators.required], nonNullable: true})
+    jsonStringInput: this.fb.control<string>('', {validators: [Validators.required], nonNullable: true}),
+    cSharpClassString: this.fb.control<string>('',{}),
+    typeScriptInterfaceString: this.fb.control<string>('',{})
   });
 
   constructor(private fb: FormBuilder){
@@ -24,33 +29,39 @@ export class AppComponent implements OnInit{
       const formattedJSON = formatJSON(inputJSON);
 
       if (!formattedJSON) {
+        this.inputForm.controls.cSharpClassString.setValue('Invalid Json String');
+        this.inputForm.controls.typeScriptInterfaceString.setValue('Invalid Json String');
         return;
       }
 
       const jsonObject = JSON.parse(formattedJSON);
       const result = this.analyzeJsonObject(jsonObject);
-      console.log(this.convertToCSharpClass(result));
+
+      console.log(result);
+      this.inputForm.controls.cSharpClassString.setValue(this.convertToCSharpClass(result));
+      this.inputForm.controls.typeScriptInterfaceString.setValue(this.convertToTypeScriptInterface(result));
     });
   }
 
-
+  resetForm():void{
+    this.inputForm.reset();
+  }
 
   analyzeJsonObject(jsonObject: any): ObjectInfo[] {
     return Object.keys(jsonObject).map((x) => {
       return {
         name: x,
         value: jsonObject[x],
-        type: typeof jsonObject[x]
+        type: Array.isArray(jsonObject[x]) ? 'array' : typeof jsonObject[x]
       } as ObjectInfo
     })
   }
 
   convertToCSharpClass(infos: ObjectInfo[]): string {
-    let resultString = 'public class ClassName {\n';
+    let resultString = 'public class ClassName\n{\n';
 
     resultString += infos.map((x) => {
-      return '\t' + `
-      ${this.modifierString} ${this.convertTypeString(x.type, x.value)} ${x.name} ${this.accessorString}`;
+      return `    ${this.modifierString} ${this.convertCSharpTypeString(x.type, x.value)} ${camelToPascal(x.name)} ${this.accessorString}`;
     }).join('\n');
     resultString += '\n}';
 
@@ -59,17 +70,61 @@ export class AppComponent implements OnInit{
     return resultString ;
   }
 
-  convertTypeString(type: string, value: any ){
+  convertToTypeScriptInterface(infos: ObjectInfo[]): string{
+    let resultString = 'export interface interfaceName {\n';
+
+    resultString += infos.map((x) => {
+      return `  ${x.name}: ${this.convertTypeScriptTypeString(x.type, x.value)};`;
+    }).join('\n');
+    resultString += '\n}';
+
+    console.log(resultString);
+
+    return resultString ;
+  }
+
+  convertCSharpTypeString(type: string, value: any ): string{
     switch (type){
       case 'number':
         return (value as number) % 1 === 0 ? 'int' : 'float';
       case 'boolean':
         return 'bool';
-      case 'Date':
-        return 'DateTimeOffset';
+      case 'string':
+        return this.testIsStringOrDate(value as string);
+      case 'array':
+        return `List<${this.convertCSharpTypeString(typeof value[0], value[0])}>`;
+      case 'null':
+        return 'string';
       default:
         return type;
     }
+  }
+
+  convertTypeScriptTypeString(type: string, value: any ): string{
+    switch (type){
+      case 'string':
+        return this.testIsStringOrDate(value as string, Language.TypeScript);
+      case 'array':
+        return `${this.convertTypeScriptTypeString(typeof value[0], value[0])}[]`;
+      case 'null':
+        return 'any';
+      default:
+        return type;
+    }
+  }
+
+  testIsStringOrDate(input: string, language: Language = Language.CSharp): string{
+    const isDateTime = this.dateTimePattern.test(input);
+    const isDateTimeOffset = this.dateTimeOffsetPattern.test(input);
+    const isUTCDateTime = this.utcDateTiimePattern.test(input);
+
+    if (language !== Language.CSharp){
+      return isDateTime || isDateTimeOffset || isUTCDateTime ? 'Date' : 'string'
+    }
+
+    return isDateTimeOffset || isUTCDateTime
+      ? 'DateTimeOffset' : isDateTime
+      ? 'DateTime' : 'string';
   }
 }
 
@@ -77,6 +132,11 @@ interface ObjectInfo{
   name: string;
   value: any;
   type: string;
+}
+
+enum Language{
+  CSharp,
+  TypeScript
 }
 
 function isValidJSON(jsonString: string): boolean {
@@ -101,4 +161,10 @@ function formatJSON(jsonString: string): string | null {
   } else {
     return null;
   }
+}
+
+function camelToPascal(input: string): string {
+  const words = input.split(/(?=[A-Z])/);
+  const pascalWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+  return pascalWords.join('');
 }
